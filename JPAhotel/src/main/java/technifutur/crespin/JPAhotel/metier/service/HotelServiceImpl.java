553e22bfff49806a6256d15a7a787c7f0217feb7
CrespinChristophe <crespin.chris@gmail.com>
2022-04-05 23:19:01 +0200

    Ajout de la méthode getOne() dans HotelServiceImpl

--- JPAhotel/src/main/java/technifutur/crespin/JPAhotel/metier/service/HotelServiceImpl.java
+++ JPAhotel/src/main/java/technifutur/crespin/JPAhotel/metier/service/HotelServiceImpl.java
@@ -1,9 +1,11 @@
 package technifutur.crespin.JPAhotel.metier.service;
 
 import org.springframework.stereotype.Service;
-import technifutur.crespin.JPAhotel.model.dto.GerantDTO;
+import technifutur.crespin.JPAhotel.data.exceptions.ElementNotFoundException;
+import technifutur.crespin.JPAhotel.data.repo.HotelRepository;
+import technifutur.crespin.JPAhotel.metier.mapper.HotelMapper;
 import technifutur.crespin.JPAhotel.model.dto.HotelDTO;
-import technifutur.crespin.JPAhotel.model.forms.GerantForm;
+import technifutur.crespin.JPAhotel.model.entities.Hotel;
 import technifutur.crespin.JPAhotel.model.forms.HotelForm;
 
 import java.util.List;
@@ -11,6 +13,13 @@ import java.util.List;
 @Service
 public class HotelServiceImpl implements HotelService{
 
+    private final HotelRepository repository;
+    private final HotelMapper mapper;
+
+    public HotelServiceImpl(HotelRepository repository, HotelMapper mapper) {
+        this.repository = repository;
+        this.mapper = mapper;
+    }
 
     @Override
     public HotelDTO insert(HotelForm form) {
@@ -18,8 +27,12 @@ public class HotelServiceImpl implements HotelService{
     }
 
     @Override
-    public HotelDTO getOne(Long id) {S
-        return null;
+    public HotelDTO getOne(Long id) {
+
+        return repository.findById(id)
+                .map(mapper::entityToDTO)
+                .orElseThrow(() -> new ElementNotFoundException(id, HotelDTO.class));
+
     }
 
     @Override
