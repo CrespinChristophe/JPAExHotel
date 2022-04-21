ef79a8d1ba9e54a59008072fc04b8f2cb657bf7c
Crespin <crespin.christophe@netc.eu>
2022-04-21 09:29:56 +0200

    dernier commit

--- JPAhotel/src/main/java/technifutur/crespin/JPAhotel/model/forms/HotelForm.java
+++ JPAhotel/src/main/java/technifutur/crespin/JPAhotel/model/forms/HotelForm.java
@@ -1,8 +1,10 @@
 package technifutur.crespin.JPAhotel.model.forms;
 
 import lombok.Data;
+import org.springframework.validation.annotation.Validated;
 
 @Data
+@Validated
 public class HotelForm {
     private String nom;
     private String adresse;
